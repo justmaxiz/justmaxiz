1503b1304328312ee907d15c5edbb9d805b1660a
justmaxiz <maxdonater12@gmail.com>
2025-03-01 10:19:30 +0400

    privet

--- maksim_kovalev/main.py
+++
@@ -1,6 +0,0 @@
-# first_list = [{'first_name': 'Tinkov', 'last_name': 'Oleg', 'age': 57}, {'first_name': 'Bilalov', 'last_name': 'Rafael', 'age': 19}]
-
-# # Необходимо найти номер списка, в котором first_name = Tinkov
-
-# for i in first_list:
-#     print('privet')
